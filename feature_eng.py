16f5cc2925feab5c0897ef113514d8ec2356f151
northcapen <8VivAWLR1j>
2018-05-06 13:17:13 +0300

    sex is most important feature for now

--- feature_eng.py
+++ feature_eng.py
@@ -1,5 +1,5 @@
 def defined_features():
-   return {'Pclass': [1, 2, 3], 'Sex': ['male', 'female'], 'PreSchool': [True, False]}
+   return {'Sex': ['male', 'female'], 'PreSchool': [True, False], 'Pclass': [1, 2, 3]}
 
 def feature_engineering(frame):
     frame['PreSchool'] = frame.apply(lambda p: p['Age'] <= 6, axis=1)
